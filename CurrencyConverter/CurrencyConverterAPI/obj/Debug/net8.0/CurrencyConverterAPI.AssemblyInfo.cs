3fedd0d1c6ce9d93cece08933e5e97445f6b5737
Kiran Kulkarni <34923686+KiranVK98@users.noreply.github.com>
2024-07-05 03:33:21 -0500

    no_changes

--- CurrencyConverter/CurrencyConverterAPI/obj/Debug/net8.0/CurrencyConverterAPI.AssemblyInfo.cs
+++ CurrencyConverter/CurrencyConverterAPI/obj/Debug/net8.0/CurrencyConverterAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CurrencyConverterAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ede47d2f64982dd8580b5ebceaf2f8ed9cb51314")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd94ad21b96a1b0346f79ffc0d3492bb336ceab0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CurrencyConverterAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CurrencyConverterAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
